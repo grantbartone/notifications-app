440bc59d0dbc5731df37076a42d0780bd9ca0ea7
Grant Bartone <grant@pared.com>
2020-09-17 17:35:02 -0700

    fetch the notifications markdown on first render

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import NotificationIcon from './NotificationIcon'
 import NotificationList from './NotificationList'
 import './App.css';
 
 function App() {
-  const [ notifications, setNotifications ] = useState(1)
-  const [ showNotificationsList, setShowNotificationsList ] = useState(false)
   const [ notificationsText, setNotificationsText ] = useState("")
-  
-  const handleNotificationClick = async () => {
-    const text = await (await fetch('https://gist.githubusercontent.com/grantbartone/f02d21544a4759f3f3b8704df0dfe908/raw/db19c8cf5c0ca4ff47f8f128a4d25665784c7083/releaseTest.md')).text()
-    setNotificationsText(text)
+  const [ notificationsCount, setNotificationsCount ] = useState(0)
+  const [ showNotificationsList, setShowNotificationsList ] = useState(false)
+
+  useEffect(() => {
+    async function getNotificationsText() {
+      const text = await (await fetch('https://gist.githubusercontent.com/grantbartone/f02d21544a4759f3f3b8704df0dfe908/raw/db19c8cf5c0ca4ff47f8f128a4d25665784c7083/releaseTest.md')).text()
+      setNotificationsText(text)
+    }
+    getNotificationsText()
     
-    // TODO: Replace with notifications count
-    setNotifications(notifications + 1)
-    setShowNotificationsList(true)
+    const count = getNotificationCount()
+    setNotificationsCount(count)
+  }, [])
+
+  const getNotificationCount = () => {
+    // TODO: Parse the Markdown file to get the notification count
+    return 2
   }
+  
+  const handleNotificationClick = () => setShowNotificationsList(true)
 
   const handleCloseModal = () => setShowNotificationsList(false)
 
@@ -23,7 +32,7 @@ function App() {
     <div className="App">
       <div>
         <NotificationIcon
-          count={notifications}
+          count={notificationsCount}
           handleNotificationClick={handleNotificationClick}
         />
         {showNotificationsList && (
