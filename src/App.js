6197c63fbb34590e3f7c9a22a984cb70ee7bb843
Grant Bartone <grant@pared.com>
2020-09-17 21:50:01 -0700

    adds an error handler if the request fails

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import './App.css';
 
 function App() {
   const [ notificationsText, setNotificationsText ] = useState("")
+  const [ showError, setShowError ] = useState(false)
   const [ notificationsCount, setNotificationsCount ] = useState(0)
   const [ showNotificationsList, setShowNotificationsList ] = useState(false)
   const [ hideNotificationsList, setHideNotificationsList ] = useState(false)
 
   useEffect(() => {
     async function getNotificationsText() {
-      const text = await (await fetch('https://gist.githubusercontent.com/grantbartone/f02d21544a4759f3f3b8704df0dfe908/raw/a80068bd536f870b2579c2f2f0d9ed850b1cd4ca/releaseTest.md')).text()
+      const response = await fetch('https://gist.githubusercontent.com/grantbartone/f02d21544a4759f3f3b8704df0dfe908/raw/a80068bd536f870b2579c2f2f0d9ed850b1cd4ca/releaseTest.md')
+      
+      if (response.status !== 200) return handleError(response)
+      const text = await response.text()
       setNotificationsText(text)
     }
     getNotificationsText()
     
+    // Show the notification icon and badge after a short delay for aesthetic effect
     const count = getNotificationCount()
     setTimeout(() => setNotificationsCount(count), 1000)
   }, [])
@@ -35,13 +40,25 @@ function App() {
     )
   }
 
+  const handleError = async (response) => {
+    const text = await response.text()
+    setNotificationsText(
+      `Whoops, something went wrong getting the Release Notes!
+      Please refresh the page to try again!\n\n${text}`
+    )
+    setShowError(true)
+    setShowNotificationsList(true)
+  }
+
   return (
     <div className="App">
       <div>
-        <NotificationIcon
-          count={notificationsCount}
-          handleNotificationClick={handleNotificationClick}
-        />
+        {!showError && (
+          <NotificationIcon
+            count={notificationsCount}
+            handleNotificationClick={handleNotificationClick}
+          />
+        )}
         {showNotificationsList && (
           <NotificationList
             hide={hideNotificationsList}
